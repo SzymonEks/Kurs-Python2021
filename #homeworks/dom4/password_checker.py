b9c7242d451780758d0899af071aafac8be13064
11001147 <Eob2007&>
2021-02-01 17:27:01 +0100

    delete while loop

--- #homeworks/dom4/password_checker.py
+++ #homeworks/dom4/password_checker.py
@@ -20,30 +20,27 @@ print('f. Maksymalna długość hasła 64 znaki')
 
 password = input("Podaj hasło do sprawdzenia: ")
 flag = 0
-while True:
-    if (len(password) < 8):
-        flag = -1
-        print("hasło za krótkie - Minimalna długość hasła 8 znaków")
-    if (len(password) > 64):
-        flag = -1
-        print("hasło za długie - Maksymalna długość hasła 64 znaki")
-    if not re.search("[a-z]", password):
-        flag = -1
-        print("Hasło nie zawiera conajmniej jednej małej litery")
-    if not re.search("[A-Z]", password):
-        flag = -1
-        print("Hasło nie zawiera conajmniej jednej wielkiej litery")
-    if not re.search("[0-9]", password):
-        flag = -1
-        print("Hasło nie zawiera conajmniej jednej cyfry")
-    if not re.search("[-@#]", password):  # czy jest jakaś funkcja na znaki specjlane? coś jak ifalpha?
-        flag = -1
-        print("Hasło nie zawiera znaku specjalnego")
-        break
-    else:
-        flag = 0
-        print("Hasło poprawne")
-        break
+if (len(password) < 8):
+    flag = -1
+    print("hasło za krótkie - Minimalna długość hasła 8 znaków")
+if (len(password) > 64):
+    flag = -1
+    print("hasło za długie - Maksymalna długość hasła 64 znaki")
+if not re.search("[a-z]", password):
+    flag = -1
+    print("Hasło nie zawiera conajmniej jednej małej litery")
+if not re.search("[A-Z]", password):
+    flag = -1
+    print("Hasło nie zawiera conajmniej jednej wielkiej litery")
+if not re.search("[0-9]", password):
+    flag = -1
+    print("Hasło nie zawiera conajmniej jednej cyfry")
+if not re.search("[-@#]", password):  # czy jest jakaś funkcja na znaki specjlane? coś jak ifalpha?
+    flag = -1
+    print("Hasło nie zawiera znaku specjalnego")
+else:
+    flag = 0
+    print("Hasło poprawne")
 
 if flag == -1:
     print("Hasło nie poprawne")
