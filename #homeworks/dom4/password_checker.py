2545e1afd50887b70776aeb53af486a58424b1bd
11001147 <Eob2007&>
2021-02-02 17:41:37 +0100

    add question as comment + comment string.punctuation

--- #homeworks/dom4/password_checker.py
+++ #homeworks/dom4/password_checker.py
@@ -1,6 +1,7 @@
 import re
 import string
 
+
 # # Napisz program, który jako dane wejściowe przyjmuje hasło od użytkownika i sprawdza
 # poprawność hasła względem poniższym kryteriów:
 # a. Conajmniej jedna mała litera
@@ -21,6 +22,7 @@ print('f. Maksymalna długość hasła 64 znaki')
 
 password = input("Podaj hasło do sprawdzenia: ")
 flag = 0
+
 if (len(password) < 8):
     flag = -1
     print("hasło za krótkie - Minimalna długość hasła 8 znaków")
@@ -36,7 +38,8 @@ if not re.search("[A-Z]", password):
 if not re.search("[0-9]", password):
     flag = -1
     print("Hasło nie zawiera conajmniej jednej cyfry")
-if not re.search("[string.punctuation]", password):
+# if not re.search(string.punctuation, password): # nie wiem jak to napisać zeby 'string.punctuation' był pattern
+if not re.search("[!#$%&'()*+,-./:;<=>?@^_`{|}~]", password):   #nie wiem jak dodać wszyskie znaki do listy - np. [] '' "" ()
     flag = -1
     print("Hasło nie zawiera znaku specjalnego")
 if flag < 0:
