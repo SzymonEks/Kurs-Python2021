a0757e9c49e7e056af22773832fe39cfd0b424e6
11001147 <Eob2007&>
2021-02-24 16:09:32 +0100

    add test_BankPrzemka.py

--- #homeworks/dom10/tests/test_BankPrzemka.py
+++ #homeworks/dom10/tests/test_BankPrzemka.py
@@ -6,7 +6,8 @@ from BankPrzemka import PromoSavingsAccount, BankSystem, SavingsAccount, BankAcc
 class AccountTestCase(TestCase):
     def setUp(self):
         self.bank_system = BankSystem()
-        self.client_1 = BankSystem.add_account('Adam', 'Małysz', '111', 10000, 'PLN',1000)
+        self.client_1 = BankAccount('Adam', 'Małysz', '111', 10000, 'PLN', 1000)
+        self.add_client_1 = BankSystem.add_account(client_1)
         # self.client_2 = BankAccount('Janusz', 'Wajs', '222', 50000, 'PLN',1000)
         # self.Savings_client_1 = SavingsAccount('Jan', 'Oszczędny', '555', 10000, 'PLN',1000, 0.1)
         # self.promo_Savings_client_1 = PromoSavingsAccount('Jan', 'Turbooszczędny', '666', 10000, 'PLN', 10000, 0.5, 500)
