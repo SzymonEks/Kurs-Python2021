32faa2ad5af4652964f151967fb48a5961fb83d9
11001147 <Eob2007&>
2021-02-24 14:31:44 +0100

    add test_BankPrzemka.py

--- #homeworks/dom10/tests/test_BankPrzemka.py
+++ #homeworks/dom10/tests/test_BankPrzemka.py
@@ -9,13 +9,14 @@ class AccountTestCase(TestCase):
         self.client_2 = BankAccount('Janusz', 'Wajs', '222', 50000, 'PLN',1000)
         self.Savings_client_1 = SavingsAccount('Jan', 'Oszczędny', '555', 10000, 'PLN',1000, 0.1)
         self.promo_Savings_client_1 = PromoSavingsAccount('Jan', 'Turbooszczędny', '666', 10000, 'PLN', 10000, 0.5, 500)
+        self.BankSystem = BankSystem
 
     def test_deposit_ok(self):
         balance_before = self.client_1.balance
-        to_deposit = 100
+        amount = 100
+        self.BankSystem.deposit('111', amount)
 
-        self.BankSystem.deposit('111', to_deposit)
 
-        expected = balance_before + to_deposit
+        expected = balance_before + amount
         self.assertEqual(expected, self.client_1.balance)
 
